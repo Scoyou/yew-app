f976d6f2d8686054a0e45b86dcd42232bd52070e
Scott Young <scott.young@getbridge.com>
2021-12-21 15:06:37 -0700

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,56 @@
+use yew::prelude::*;
+
+enum Msg {
+    AddOne,
+    SubtractOne,
+}
+
+struct Greeting { value: String }
+
+struct Model {
+    value: i64,
+}
+
+impl Component for Model {
+    type Message = Msg;
+    type Properties = ();
+
+    fn create(_ctx: &Context<Self>) -> Self {
+        Self {
+            value: 0,
+        }
+    }
+
+    fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
+        match msg {
+            Msg::AddOne => {
+                self.value += 1;
+                // the value has changed so we need to
+                // re-render for it to appear on the page
+                true
+            },
+            Msg::SubtractOne => {
+                self.value -= 1;
+                true
+            }
+        }
+    }
+
+    fn view(&self, ctx: &Context<Self>) -> Html {
+        // This gives us a component's "`Scope`" which allows us to send messages, etc to the component.
+        let link = ctx.link();
+        let greeting = Greeting { value: "Hello Counter!".to_string() };
+        html! {
+            <div>
+                <h1>{ greeting.value }</h1>
+                <button onclick={link.callback(|_| Msg::AddOne)}>{ "+1" }</button>
+                <button onclick={link.callback(|_| Msg::SubtractOne)}>{ "-1" }</button>
+                <p>{ self.value }</p>
+            </div>
+        }
+    }
+}
+
+fn main() {
+    yew::start_app::<Model>();
+}
